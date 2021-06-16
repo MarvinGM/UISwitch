277355f18eb89ee2f09280354c7db172d0ba9849
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-16 15:58:23 +0800

    Version 1.0

--- UISwitch/ViewController.swift
+++ UISwitch/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var mySwitch: UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func switchDidChange(_ sender: UISwitch) {
+        if sender.isOn {
+            view.backgroundColor = .red
+        }
+        else {
+            view.backgroundColor = .blue
+        }
     }
 
 
