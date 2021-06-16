04286c48b861795a10d1f1d4f90a1db0536299fa
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-16 15:49:46 +0800

    Initial Commit

---
+++ UISwitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISwitch
+//
+//  Created by Amben on 6/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
